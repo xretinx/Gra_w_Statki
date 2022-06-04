461909cab28943d6bfb24242298ea85f2e991bec
BuildTools <unconfigured@null.spigotmc.org>
2022-06-04 14:40:41 +0200

    Pusty projekt z sfml

--- Gra_w_Statki/Gra_w_Statki.cpp
+++ Gra_w_Statki/Gra_w_Statki.cpp
@@ -1,8 +1,7 @@
 ﻿// Gra_w_Statki.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 #include <iostream>
-
+#include"SFML/Graphics.hpp"
 int main()
 {
-    srand(time(NULL));
-
+    
 }
