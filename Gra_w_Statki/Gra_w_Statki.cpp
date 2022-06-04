d2b622350744e774bb4e73f9faa36f21585b4760
BuildTools <unconfigured@null.spigotmc.org>
2022-06-04 14:19:39 +0200

    assets

--- Gra_w_Statki/Gra_w_Statki.cpp
+++ Gra_w_Statki/Gra_w_Statki.cpp
@@ -1,11 +1,8 @@
 ﻿// Gra_w_Statki.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(NULL));
+
 }
-//test czy dziala
-//visual studio 2019
